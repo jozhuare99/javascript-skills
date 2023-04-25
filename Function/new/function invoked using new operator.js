a01271f8f0a0c967b47406264e6575b08aa5c3ce
jozhuare99 <jozhuare99@gmail.com>
2023-04-25 16:03:15 +0800

    add a Jinvoked in new operator on function

---
+++ Function/new/function invoked using new operator.js
@@ -0,0 +1,12 @@
+function Person(name, age){
+  this.name = name
+  this.age = age
+}
+
+Person.prototype.sayHello = function(){
+  console.log("Hello, my name is " + this.name)
+}
+
+const john john = new Person("John", 25)
+
+john.sayHello(); //Hello, my name is John
