dcad9ba01cac1ea17e22f0b78606311f0157a508
jozhuare99 <jozhuare99@gmail.com>
2023-04-26 13:04:08 +0800

    complete the function constructor

--- Function/Function Constructor/5. Examples.js
+++ Function/Function Constructor/5. Examples.js
@@ -14,3 +14,28 @@ function Person(name, age){ // function constructor that have properties
 Person.prototype.sayHello = function(){
   console.log(`Hello, my name is ${this.name}` )
 }
+
+//create a function object from a function declaration or function expression
+
+const sumOfArray = new Function("const sumArray = (arr) => arr.reduce((previousValue, currentValue) => previousValue + currentValue); return  sumArray")
+console.log(sumOfArray.call({}).call({},[1,2,3,4])) //10
+
+
+// this is unsafe shit 
+
+
+
+const findLargestNumber = new Function(
+  "function findLargestNumber (arr) { return Math.max(...arr) }; return findLargestNumber",
+);
+
+
+console.log(findLargestNumber.call({}).call({}, [2, 4, 1, 8, 5])); // 8
+
+
+
+const sayHello = new Function(
+  "return function(name){return `hello, ${name}`}"
+)
+
+console.log(sayHello.call({}).call({},"world"))
--- test.js
+++ test.js
@@ -1,13 +1,22 @@
+const sumOfArray = new Function("const sumArray = (arr) => arr.reduce((previousValue, currentValue) => previousValue + currentValue); return  sumArray")
+console.log(sumOfArray.call({}).call({},[1,2,3,4])) //10
 
-const recursiveFn = new Function("count", `
-(function recursiveFn(count){
-  if(count < 0){
-    return
-  }
-  console.log(count)
-  recursiveFn(count - 1)
-})(count)
-`,
+
+// this is unsafe shit 
+
+
+
+const findLargestNumber = new Function(
+  "function findLargestNumber (arr) { return Math.max(...arr) }; return findLargestNumber",
 );
 
-console.log(recursiveFn(3))
+
+console.log(findLargestNumber.call({}).call({}, [2, 4, 1, 8, 5])); // 8
+
+
+
+const sayHello = new Function(
+  "return function(name){return `hello, ${name}`}"
+)
+
+console.log(sayHello.call({}).call({},"world"))
